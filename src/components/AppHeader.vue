4713c698d96560d38b34447f4c4bcfea71887d3f
didemertens <d.g.j.mertens@gmail.com>
2020-04-12 15:36:48 +0100

    add styling to header menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    App header
+  <div class="ui tabular menu">
+    <a href="/" class="item active">
+      Home
+    </a>
+    <div class="right menu">
+      <a class="ui item">
+        Logout
+      </a>
+    </div>
   </div>
 </template>
 
