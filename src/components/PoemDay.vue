08e40afb04d4d813cec56ea19e214657e3c5d19a
didemertens <d.g.j.mertens@gmail.com>
2020-04-12 18:40:34 +0100

    add vue router with paths for search and poem of the day

---
+++ src/components/PoemDay.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Poem of the day</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PoemDay'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App'
 import store from './store'
+import SearchBar from "./components/SearchBar"
+import PoemDay from './components/PoemDay'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/search', component: SearchBar },
+    { path: '/poemofday', component: PoemDay }
+  ]
+})
 
 new Vue ({
+  router,
   store,
   render: h => h(App)
 }).$mount('#app')
