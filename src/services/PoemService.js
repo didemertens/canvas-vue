a8b87912848e3c2f4e4468f0ed7b5690fce9add0
didemertens <d.g.j.mertens@gmail.com>
2020-04-14 13:17:22 +0100

    deploy express server

--- src/services/PoemService.js
+++ src/services/PoemService.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default {
   async getPoems(req) {
-    let res = await axios.get(`http://localhost:5000/poems/${req.search}/${req.filter}`)
+    let res = await axios.get(`http://localhost:8080/poems/${req.search}/${req.filter}`)
     return res.data
   }
 }
