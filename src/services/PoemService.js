07b313cabee6a3c79ae9393422ec83642b3d1f3a
didemertens <d.g.j.mertens@gmail.com>
2020-04-14 13:19:35 +0100

    deploy express server

--- src/services/PoemService.js
+++ src/services/PoemService.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default {
   async getPoems(req) {
-    let res = await axios.get(`http://localhost:8080/poems/${req.search}/${req.filter}`)
+    let res = await axios.get(`https://poemsfinder.herokuapp.com/poems/${req.search}/${req.filter}`)
     return res.data
   }
 }
