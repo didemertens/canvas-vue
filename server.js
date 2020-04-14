14201f0f28bbbac4e3668babc6d1626c0de365eb
didemertens <d.g.j.mertens@gmail.com>
2020-04-14 13:09:45 +0100

    deploy express server

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.get('/poems/:search/:filter', async (req, res) => {
 
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 
-const port = process.env.PORT || 5000
-app.listen(port, () => console.log('Static server on port 5000'))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log('Static server on port 5000'))
